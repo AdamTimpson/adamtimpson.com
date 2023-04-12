cdebe82c3b0d4e0c6bfc5cebd88a5d1323aa5d57
Adam Timpson <adamtimpson@gmail.com>
2023-04-12 22:09:31 +0100

    Fresh start

--- frontend/home/src/app/app.module.ts
+++ frontend/home/src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { IonicModule } from '@ionic/angular';
-import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    MainComponent,
-    HeaderComponent,
-    FooterComponent,
-    SlideComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    IonicModule.forRoot()
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
